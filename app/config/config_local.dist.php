07832849bfb0113fa8042fb35393b13cd3bef32c
victor <victor.odiah@grgme.com>
2018-01-12 15:08:57 +0000

    password reset functionality ok

---
+++ app/config/config_local.dist.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: odiahv
+ * Date: 09/01/2018
+ * Time: 17:15.
+ */
+return [
+];
---
+++ app/config/parameters_local.dist.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: odiahv
+ * Date: 11/01/2018
+ * Time: 14:23.
+ */
+
+return [
+    'mautic.cookie_path' => '/',
+];
